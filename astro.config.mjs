71fc3504d52ebc3df503bf5ceaf2170bf551813b
kashu <64694079+kashu-02@users.noreply.github.com>
2024-05-18 10:19:47 +0900

    fix function

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
 						{ label: '02-Goの基礎', link: '/lec01/02-go-intro' },
 						{ label: '03-条件分岐', link: '/lec01/03-conditional-branch' },
 						{ label: '04-ループ', link: '/lec01/04-loop' },
-						{ label: '05-関数', link: '/lec01/05-関数' },
+						{ label: '05-関数', link: '/lec01/05-function' },
 						{ label: '06-構造体と配列', link: '/lec01/06-structs-array' },
 						{ label: '07-パッケージとインポート', link: '/lec01/07-packages-imports' },
 					],
